8a0cc8620de9f4b878716aed856790022610e1a3
tranrm3 <tranrm3@uci.edu>
2024-01-27 14:34:04 -0800

    added noti screen

---
+++ lib/screens/notification_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const NotificationScreen(),
+    );
+  }
+}
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
