cce734d16f226b9f99fc6c043f9cd29d110e1694
Sean Liem <seanyliem@gmail.com>
2024-01-27 13:17:41 -0800

    added list and item classes

---
+++ lib/classes/lists.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class List
+{
+  late String id;
+  late String name;
+  late String color;
+  late String imgUrl;
+
+
+  List({
+    required this.id,
+    required this.name,
+    required this.color,
+    this.imgUrl = ""
+    });
+
+  //method to turn firestore data into a listing
+  factory List.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot, SnapshotOptions? options,)
+  {
+    final data = snapshot.data();
+    return List{
+      id: '';
+      name: data!['name'],
+      color: data!['color'],
+      imgUrl: data!['imgUrl']
+    };
+  }
+
+  Map<String, dynamic> toFirestore()
+  {
+    return {
+      'name': name,
+      'color': color,
+      'imgUrl' : imgUrl
+    };
+  }
+}
