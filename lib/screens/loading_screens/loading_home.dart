54ab535bd1a328b91aed1a6e5849d5f1d4e7a244
juliusya45 <derpyjul45@gmail.com>
2024-01-27 14:58:31 -0800

    fixed pubspec and fetching backend stuff for loading screen works

--- lib/screens/loading_screens/loading_home.dart
+++ lib/screens/loading_screens/loading_home.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:hack_grocery_app/classes/group.dart';
 import 'package:hack_grocery_app/classes/user.dart';
 import 'package:hack_grocery_app/screens/authentication/auth_page.dart';
+import 'package:hack_grocery_app/screens/groups_screen.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LoadingHome extends StatefulWidget {
@@ -20,7 +21,23 @@ class _LoadingHomeState extends State<LoadingHome> {
   List<Group> allGroups = [];
   late AppUser sendUser;
 
+  Future<void> getUserData() async{
+    final docName = FirebaseAuth.instance.currentUser?.uid;
+    final userRef = database.collection('users').doc(docName).withConverter(
+      fromFirestore: AppUser.fromFirestore, 
+      toFirestore: (AppUser appuser, _) => appuser.toFirestore()
+      );
+
+    await userRef.get().then((value) {
+      sendUser = value.data()!;
+      print('got user!');
+      //this prints out data from the db correctly
+      print(sendUser.username);
+    });
+  }
+
   Future<void> setup() async{
+    await getUserData();
     //defining the reference to our database:
     final ref = database.collection('groups').orderBy('time').withConverter(
     fromFirestore:  Group.fromFirestore,
@@ -34,10 +51,33 @@ class _LoadingHomeState extends State<LoadingHome> {
       for(var doc in event.docs)
       {
         final groups = doc.data();
-        if(groups != null && groups.id == sendUser.groups.contains(groups.id))
+        if(groups != null && sendUser.groups.contains(groups.id))
         {
+            //giving the groups object an id since it was left out originally
+            groups.id = doc.id;
+            allGroups.add(groups);
         }
       }
+    });
+  }
+
+  @override
+  void initState()
+  {
+    super.initState();
+    setup().then((result)
+    {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if(sendUser != null)
+        {
+          Navigator.pushReplacement(context,
+           MaterialPageRoute(
+            settings: const RouteSettings(name: '/groups'),
+            builder: (context) => GroupScreen()
+            ),
+           );
+        }
+      });
     });
   }
 
