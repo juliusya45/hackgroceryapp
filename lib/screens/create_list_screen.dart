eb5f865e76a1fc3f57f7c54ae4da0630bfcbba1f
Kennethdao18 <kennethdao18@gmail.com>
2024-01-27 22:07:42 -0800

    new dart file

---
+++ lib/screens/create_list_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hack_grocery_app/screens/logout_screen.dart';
+import 'package:hack_grocery_app/screens/notification_screen.dart';
+
+class CreateList extends StatefulWidget {
+  const CreateList({super.key});
+
+  @override
+  State<CreateList> createState() => _CreateListState();
+}
+
+class _CreateListState extends State<CreateList> {
+
+  final _listNameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+      backgroundColor: Colors.green,
+      title: Text('Create New List'),
+      centerTitle: true,
+  ),
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [ 
+        Padding(
+          padding: const EdgeInsets.all(25),
+            child: TextField(
+            controller: _listNameController,
+            decoration: InputDecoration(
+            hintText: "Enter List Name"
+          ),
+        ),
+      ),
+        ElevatedButton(
+        onPressed : () {},
+        child: Text('Create!'),
+          ),
+        ],
+      ),
+      ),
+    );
+  }
+}
