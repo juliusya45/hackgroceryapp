c92d99594bd870726641dae89cf9fdb69c7fe8c4
tranrm3 <tranrm3@uci.edu>
2024-01-28 08:59:21 -0800

    test

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -66,3 +66,49 @@ class _NavState extends State<Nav> {
     );
   }
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, required this.groupsList, required this.appUser});
+  final List<Group> groupsList;
+  final AppUser appUser;
+  @override   
+  Widget build(BuildContext context) {
+    groupsList = widget.groupsList;
+    appUser = widget.appUser;
+    return PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: Colors.white, // Default is Colors.white.
+        handleAndroidBackButtonPress: true, // Default is true.
+        resizeToAvoidBottomInset: true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+        stateManagement: true, // Default is true.
+        hideNavigationBarWhenKeyboardShows: true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: Colors.white,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: ItemAnimationProperties( // Navigation Bar's items animation properties.
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: ScreenTransitionAnimation( // Screen transition animation on change of selected tab.
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle: NavBarStyle.style1, // Choose the nav bar style with this property.
+    );
+  }
+  
+}
+  List<Widget> _buildScreens() {
+        return [
+          GroupScreen(),
+          SettingsScreen()
+        ];
+    }
