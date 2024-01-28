d83974612d79f4135e4f24f9fb6d643dca1d3cb4
juliusya45 <derpyjul45@gmail.com>
2024-01-27 16:17:18 -0800

    added empty variables for user and other stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hack_grocery_app/classes/group.dart';
+import 'package:hack_grocery_app/classes/group_card.dart';
+import 'package:hack_grocery_app/classes/user.dart';
 import 'package:hack_grocery_app/firebase_options.dart';
 import 'package:hack_grocery_app/screens/authentication/login_screen.dart';
 import 'package:hack_grocery_app/screens/authentication/signup_screen.dart';
@@ -28,6 +31,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //objects that hold empty data:
+    var emptyGroup = Group(color: '', id: '', name: '', imgUrl: '');
+    var emptyUser = AppUser(uid: '', username: '', email: '', groups: []);
+
+
+
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Inter',useMaterial3: true),
       initialRoute: '/',
@@ -36,7 +46,8 @@ class MainApp extends StatelessWidget {
         '/': (context) => const InitialScreen(),
         '/groups': (context) => const GroupScreen(),
         '/verification': (context) => const VerificationScreen(),
-      '/loading_home': (context) => const LoadingHome()
+        '/loading_home': (context) => const LoadingHome(),
+        '/group_card': (context) => GroupCard(groupItem: emptyGroup),
       },
     );
   }
